02b2447369c4b589dd1438945ba10c0e219c9bf7
曾侯祺 <arkey@sz-xunjie.com>
2020-09-15 18:17:59 +0800

    feat: 用户信息格式校验

---
+++ src/middlewares/validator.js
@@ -0,0 +1,34 @@
+/**
+ * @description json schema 验证中间件
+ * @author arkey
+ */
+
+const { ErrorModel } = require('../model/ResModel')
+const {
+  jsonSchemaFileInfo
+} = require('../model/ErrorInfo')
+
+/**
+ * 生成 json schema 验证的中间件
+ * @param {function} validateFn 验证函数
+ */
+function genValidator(validateFn) {
+  // 定义中间件函数
+  async function validator(ctx, next) {
+    const data = ctx.request.body
+    const error = validateFn(data)
+    if (error) {
+      // 校验失败
+      ctx.body = new ErrorModel(jsonSchemaFileInfo)
+      return
+    }
+    // 校验成功，继续
+    await next()
+  }
+  // 返回中间件
+  return validator
+}
+
+module.exports = {
+  genValidator
+}
