e7310672ec8e4c0e1141b3ce1b2977798ee1315a
曾侯祺 <arkey@sz-xunjie.com>
2020-09-16 17:48:58 +0800

    feat: 修改用户信息接口 和 退出登录接口

--- src/controller/user.js
+++ src/controller/user.js
@@ -3,14 +3,21 @@
  * @author arkey
  */
 
-const { getUserInfo, createUser, deleteUser } = require('../services/user')
+const { 
+  getUserInfo, 
+  createUser, 
+  deleteUser,
+  updateUser
+} = require('../services/user')
 const { SuccessModel, ErrorModel } = require('../model/ResModel')
 const { 
   registerUserNameNotExistInfo,
   registerUserNameExistInfo,
   registerFailInfo,
   loginFailInfo,
-  deleteUserFailInfo
+  deleteUserFailInfo,
+  changeInfoFailInfo,
+  changePasswordFailInfo
 } = require('../model/ErrorInfo')
 const doCrypto = require('../utils/cryp')
 
@@ -28,7 +35,7 @@ async function isExist(userName) {
 }
 
 /**
- * 用户注册
+ * 注册
  * @param {string} userName 用户名
  * @param {string} password 密码
  * @param {string} gender 性别
@@ -55,7 +62,7 @@ async function register({ userName, password, gender }) {
 }
 
 /**
- * 用户登录
+ * 登录
  * @param {Object} ctx koa2 ctx
  * @param {string} userName 用户名
  * @param {string} password 密码
@@ -90,9 +97,76 @@ async function deleteCurUser(userName) {
   return new ErrorModel(deleteUserFailInfo)
 }
 
+/**
+ * 修改个人信息
+ * @param {Object} ctx ctx 
+ * @param {string} nickName 昵称 
+ * @param {string} city 城市 
+ * @param {string} picture 头像 
+ */
+async function changeInfo(ctx, { nickName, city, picture }) {
+  const { userName } = ctx.session.userInfo
+  if (!nickName) {
+    nickName = userName
+  }
+
+  const result = await updateUser(
+    { 
+      newNickName: nickName,
+      newCity: city,
+      newPicture: picture
+    },
+    { userName }
+  )
+
+  if (result) {
+    // 执行成功
+    Object.assign(ctx.session.userInfo, {
+      nickName,
+      city,
+      picture
+    })
+    // 返回
+    return new SuccessModel()
+  }
+  // 失败
+  return new ErrorModel(changeInfoFailInfo)
+}
+
+/**
+ * 修改密码
+ * @param {string} userName 用户名 
+ * @param {string} password 旧密码 
+ * @param {string} newPassword 新密码 
+ */
+async function changePassword(userName, password, newPassword) {
+  const result = await updateUser(
+    { newPassword: doCrypto(newPassword) },
+    { 
+      userName, 
+      password: doCrypto(password)
+    }
+  )
+
+  if (result) {
+    // 成功
+    return new SuccessModel()
+  }
+  // 失败
+  return new ErrorModel(changePasswordFailInfo)
+}
+
+async function logout(ctx) {
+  await delete ctx.session.userInfo
+  return new SuccessModel()
+}
+
 module.exports = {
   isExist,
   register,
   login,
-  deleteCurUser
+  deleteCurUser,
+  changeInfo,
+  changePassword,
+  logout
 }
