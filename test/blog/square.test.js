f689d9373a857e26bf4fb674fc7c695a8fd4b9c9
曾侯祺 <arkey@sz-xunjie.com>
2020-09-17 16:51:45 +0800

    test: 广场 单元测试

---
+++ test/blog/square.test.js
@@ -0,0 +1,22 @@
+/**
+ * @description 广场 test
+ * @author arkey
+ */
+
+const server = require('../server')
+const { COOKIE } = require('../testUserInfo')
+
+// 加载第一页数据
+test('广场页，加载第一页数据，应该成功', async () => {
+  const res = await server
+    .get(`/api/square/loadMore/0`)
+    .set('cookie', COOKIE)
+  expect(res.body.errno).toBe(0)
+
+  const data = res.body.data
+  expect(data).toHaveProperty('isEmpty')
+  expect(data).toHaveProperty('blogList')
+  expect(data).toHaveProperty('pageSize')
+  expect(data).toHaveProperty('pageIndex')
+  expect(data).toHaveProperty('count')
+})
