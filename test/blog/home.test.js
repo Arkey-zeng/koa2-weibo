0eb85048fa4aaaaaabf9e74c358d8c653f5172fa
曾侯祺 <arkey@sz-xunjie.com>
2020-09-18 17:53:55 +0800

    test: 首页

--- test/blog/home.test.js
+++ test/blog/home.test.js
@@ -29,3 +29,18 @@ test('创建一条微博，应该成功', async () => {
   // 记录微博 id
   BLOG_ID = res.body.data.id
 })
+
+// 加载第一页数据
+test('首页，加载第一页数据，应该成功', async () => {
+  const res = await server
+    .get(`/api/blog/loadMore/0`)
+    .set('cookie', Z_COOKIE)
+  expect(res.body.errno).toBe(0)
+
+  const data = res.body.data
+  expect(data).toHaveProperty('isEmpty')
+  expect(data).toHaveProperty('blogList')
+  expect(data).toHaveProperty('pageSize')
+  expect(data).toHaveProperty('pageIndex')
+  expect(data).toHaveProperty('count')
+})
