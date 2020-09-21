7f6c0a179a0ece4f4c1d43cccbe71810317a3b66
曾侯祺 <arkey@sz-xunjie.com>
2020-09-21 17:34:19 +0800

    线上部署

--- src/routes/view/error.js
+++ src/routes/view/error.js
@@ -5,6 +5,14 @@
 
 const router = require('koa-router')()
 
+// 故意制造一个错误
+router.get('/get-an-error', async (ctx, next) => {
+  throw Error()
+  ctx.body = {
+    msg: 'xxx'
+  }
+})
+
 router.get('/error', async (ctx, next) => {
   await ctx.render('404', {})
 })
