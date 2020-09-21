08ef835d18f7e589b3a542d4d599504b6be567fb
曾侯祺 <arkey@sz-xunjie.com>
2020-09-21 14:42:44 +0800

    feat: 开发 atMe 页面

--- src/controller/blog-at.js
+++ src/controller/blog-at.js
@@ -3,8 +3,9 @@
  * @author arkey
  */
 
-const { getAtRelationCount } = require('../services/at-relation')
+const { getAtRelationCount, getAtUserBlogList } = require('../services/at-relation')
 const { SuccessModel } = require('../model/ResModel')
+const { PAGE_SIZE } = require('../conf/constant')
 
 /**
   * 获取 @ 我的微博数量
@@ -15,6 +16,29 @@ async function getAtMeCount(userId) {
   return new SuccessModel({ count })
 }
 
+/**
+ * 获取 @ 用户的微博列表
+ * @param {number} userId userId
+ * @param {number} pageIndex page index
+ */
+async function getAtMeBlogList(userId, pageIndex = 0) {
+  const result = await getAtUserBlogList({
+    userId,
+    pageIndex,
+    pageSize: PAGE_SIZE
+  })
+  const { count, blogList } = result
+
+  return new SuccessModel({
+    isEmpty: blogList.length === 0,
+    blogList,
+    pageSize: PAGE_SIZE,
+    pageIndex,
+    count
+  })
+}
+
 module.exports = {
-  getAtMeCount
+  getAtMeCount,
+  getAtMeBlogList
 }
--- src/routes/view/blog.js
+++ src/routes/view/blog.js
@@ -10,7 +10,7 @@ const { getSquareBlogList } = require('../../controller/blog-square')
 const { getFans, getFollowers } = require('../../controller/user-relation')
 const { isExist } = require('../../controller/user')
 const { getHomeBlogList } = require('../../controller/blog-home')
-const { getAtMeCount } = require('../../controller/blog-at')
+const { getAtMeCount, getAtMeBlogList } = require('../../controller/blog-at')
 
 // 首页
 router.get('/', loginRedirect, async (ctx, next) => {
@@ -144,6 +144,36 @@ router.get('/square', loginRedirect, async (ctx, next) => {
       count
     }
   })
+})
+
+// atMe 路由
+router.get('/at-me', loginRedirect, async (ctx, next) => {
+  const { id: userId } = ctx.session.userInfo
+
+  // 获取 @ 数量
+  const atCountResult = await getAtMeCount(userId)
+  const { count: atCount } = atCountResult.data
+
+  // 获取第一页列表
+  const result = await getAtMeBlogList(userId)
+  const { isEmpty, blogList, pageSize, pageIndex, count } = result.data
+
+  // 渲染页面
+  await ctx.render('atMe', {
+    atCount,
+    blogData: {
+      isEmpty,
+      blogList,
+      pageSize,
+      pageIndex,
+      count
+    }
+  })
+
+  // 标记为已读
+  if (atCount > 0) {
+
+  }
 })
 
 module.exports = router
